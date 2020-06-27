ae605d3935dfe8f55146f145aa0bf07fb93c21e9
Jonhyfun <Jonhyfun@gmail.com>
2020-06-27 00:35:04 -0300

    'Implementando o SDK Firebase em todas as funções do forum'

--- src/routes/ForumPost/controller.js
+++ src/routes/ForumPost/controller.js
@@ -1,84 +1,71 @@
 const moment = require('moment-timezone');
 const fetch = require("node-fetch");
-const firebase = require('../../firebaseConfig');
+const firebaseConfig = require('../../firebaseConfig');
+var firebase = require("firebase/app");
+require("firebase/database");
 
-exports.postList = async (req, res) => {
-  let response = await fetch(Url().posts);
-  let data = await response.json();
-  return success(data,res);
+
+firebase.initializeApp(firebaseConfig.config);
+var database = firebase.database();
+
+
+exports.postList = async (req, res) => { //não "postarlist", Lista de post(s)
+  var x = await firebase.database().ref('/Posts/');
+  x.once('value',function(x){
+	  let resposta =  JSON.stringify(x.val());
+	  success(resposta,res);
+  });
 }
 
 exports.enviarPost = async (req,res) => {
   var id = Math.floor(1000 + Math.random() * 9000);
-  let response = await fetch(Url(id).singlePost, { method: 'PATCH', body: req.body,});
-  //let data = await response.json();
-  return success(null,res);
+  var post = JSON.parse(req.body);
+  post.postID = id; 
+  firebase.database().ref('Posts/' + id).set(post);
+  success(null,res);
 }
 
 exports.singlePost = async (req, res) => {
   var PostID = req.params.id;
-  var erro = "Post não existe";
-  return await Fetch(req,res,Url(PostID).singlePost,erro);
+  var x = await firebase.database().ref('/Posts/'+PostID);
+  x.on('value',function(x){
+	  let resposta =  JSON.stringify(x.val());
+	  success(resposta,res);
+  });
+  
 }
 
 exports.Answers = async (req, res) => {
   var PostID = req.params.id;
-  var erro = "Resposta ao post não existe";
-  return await Fetch(req,res,Url(PostID).respostas,erro);
+  var x = await firebase.database().ref('/Respostas/'+PostID);
+  x.once('value',function(x){
+	  let resposta =  JSON.stringify(x.val());
+	  success(resposta,res);
+  });
+}
+
+exports.enviarResposta = async (req, res) => {
+  var id = Math.floor(1000 + Math.random() * 9000);
+  var PostID = req.params.id;
+  firebase.database().ref('/Respostas/'+PostID+"/"+id).set(JSON.parse(req.body));
+  success(null,res);
+}
+
+exports.updateRank = async (req,res) => {
+  var PostID = req.params.id;
+  var x = await firebase.database().ref('/Posts/'+PostID+"/rank/count");
+  x.once('value',function(x){
+	  firebase.database().ref('Posts/' + PostID + "/rank").set({count: (parseInt(x.val())+1)});
+	  success(parseInt(x.val()+1),res);
+  });
 }
 
 function success(data, res){
 return res.status(200).json({
     status: true,
-	data: JSON.stringify(data),
+	data: data,
     error: false,
     reason: null,
     message: `Mensagem voltada em ${moment().tz('America/Sao_Paulo').format('DD/MM/YYYY HH:mm:ss')}`
   });
-}
-
-async function Fetch(req,res,url) {
-let data;
-  try  {
-	let response = await fetch(url);
-	data = await response.json();
-  } catch{}
-  
-  var status = 200;
-  var reason = null;
-  var error = false;
-  
-  if(data == null) {
-	status = 500;
-	error = true;
-	reason = erro;
-  }
-    return res.status(status).json({
-    status: true,
-	data: JSON.stringify(data),
-    error: error,
-    reason: reason,
-    message: `Mensagem voltada em ${moment().tz('America/Sao_Paulo').format('DD/MM/YYYY HH:mm:ss')}`
-  });
-}
-
-//o dry mais lindo q eu ja fiz na minha vida
-
-function Url(PostID) {
-return {
- posts : PostUrl(),
- singlePost : PostUrl(PostID),
- respostas : RespostaUrl(PostID)
-};
-}
-
-function PostUrl(PostID) {
-	if(PostID){
-	return firebase.config.databaseURL+"/Posts/"+PostID+"/.json";	
-	}
-return firebase.config.databaseURL+"/Posts/.json";
-}
-
-function RespostaUrl(PostID) {
-return firebase.config.databaseURL+"/Respostas/"+PostID+"/.json";
 }
--- src/routes/ForumPost/index.js
+++ src/routes/ForumPost/index.js
@@ -4,6 +4,8 @@ const controller = require('./controller');
 router.get('/postList', controller.postList); 
 router.post('/postList', controller.enviarPost);
 router.get('/singlePost/:id', controller.singlePost);
+router.get('/singlePost/:id/UpdateRank', controller.updateRank);
+router.post('/singlePost/:id/Answers',controller.enviarResposta);
 router.get('/singlePost/:id/Answers', controller.Answers);
 
 module.exports = router;
